8a31d8698563681006bf06962d49a1ac3bc4d074
Ivan Tonkikh <vanya.tonkikh666@gmail.com>
2023-08-19 14:34:21 +0300

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,8 @@
 import { fileURLToPath } from 'url';
 import { join, dirname } from 'path';
 import { test, expect } from '@jest/globals';
-import genDiff from '../src/index.js';
 import fs from 'fs';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,8 +11,8 @@ const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filen
 const getRightAnswer = (rigtAnswerFilename) => fs.readFileSync(getFixturePath(rigtAnswerFilename), 'utf-8').trim();
 
 test('comparing flat json-files', () => {
-    const firstPath = getFixturePath('file1.json');
-    const secondPath = getFixturePath('file2.json');
+  const firstPath = getFixturePath('file1.json');
+  const secondPath = getFixturePath('file2.json');
 
-    expect(genDiff(firstPath, secondPath)).toEqual(getRightAnswer('rightAnswerJSON.txt'));
+  expect(genDiff(firstPath, secondPath)).toEqual(getRightAnswer('rightAnswerJSON.txt'));
 });
